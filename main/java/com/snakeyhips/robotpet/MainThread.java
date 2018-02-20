1093458cbce39abf5cad5bb32841318007ef0cca
Jake Gough <super.jake@hotmail.co.uk>
2018-02-20 10:25:03 +0000

    Update MainThread.java

--- main/java/com/snakeyhips/robotpet/MainThread.java
+++ main/java/com/snakeyhips/robotpet/MainThread.java
@@ -3,6 +3,8 @@ package com.snakeyhips.robotpet;
 public class MainThread extends Thread {
     private SurfaceHolder surfaceHolder;
     private GameView gameView;
+    private boolean running;
+    public static Canvas canvas;
 
     public MainThread(SurfaceHolder surfaceHolder, GameView gameView) {
 
@@ -11,4 +13,31 @@ public class MainThread extends Thread {
         this.gameView = gameView;
 
     }
+    
+    @Override
+    public void run() {
+        while (running) {
+            canvas = null;
+
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized(surfaceHolder) {
+                    this.gameView.update();
+                    this.gameView.draw(canvas);
+                }
+            } catch (Exception e) {} finally {
+                if (canvas != null) {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    
+    public void setRunning(boolean isRunning) {
+    running = isRunning;
+}
 }
