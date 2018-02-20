040aee064571851cdf7a26bd8e7dbe12fd3e3115
Jake Gough <super.jake@hotmail.co.uk>
2018-02-20 09:47:34 +0000

    Update MainActivity.java
    added imports

--- main/java/com/snakeyhips/robotpet/MainActivity.java
+++ main/java/com/snakeyhips/robotpet/MainActivity.java
@@ -2,6 +2,13 @@ package com.snakeyhips.robotpet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     
