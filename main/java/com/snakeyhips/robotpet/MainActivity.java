c821ab0881abb367636a42983e7a33cdc7286762
Jake Gough <super.jake@hotmail.co.uk>
2018-02-20 09:43:07 +0000

    Update MainActivity.java
    put in options menu and shared pref stuff for saving/initialising robot object

--- main/java/com/snakeyhips/robotpet/MainActivity.java
+++ main/java/com/snakeyhips/robotpet/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private Robot mRobot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,26 @@ public class MainActivity extends AppCompatActivity {
         
         //Set ActionBar Title
         getSupportActionBar().setTitle("Title Test");
+        
+        //Retrieve previous data from SharedPreferences
+        try{
+            SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+            
+            mRobot.setId(UUID.fromString(sharedPref.getString("mRobotId"), ""));
+            mRobot.setName(sharedPref.getString("mRobotName"), ""));
+            mRobot.setAge(sharedPref.getInt("mRobotAge"), 0));
+            mRobot.setHappy(sharedPref.getInt("mRobotHappy"), 0));
+            mRobot.setHunger(sharedPref.getInt("mRobotHunger"), 0));
+            mRobot.setFatigue(sharedPref.getInt("mRobotFatigue"), 0));
+            mRobot.setNaughty(sharedPref.getInt("mRobotNaughty"), 0));
+            mRobot.setWaste(sharedPref.getInt("mRobotWaste"), 0));
+            mRobot.setAge(sharedPref.getInt("mRobotAge"), 0));
+            mRobot.setIllness(sharedPref.getBoolean("mRobotIllness", false));
+        } catch (Exception e){
+            e.printStackTrace();
+            //Initialise mRobot
+            mRobot = new Robot();
+        }
         
     }
     
@@ -30,6 +52,28 @@ public class MainActivity extends AppCompatActivity {
             case R.id.mSettingsAbout:
                 //do stuff
                 return true;
+        }
+    }
+    
+    //Saves state of Robot when activity is paused
+    @Override
+    public void onPause(){
+        super.onPause();
+        
+        if(mRobot != null){
+            SharedPreferences mainPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = mainPref.edit();
+            
+            editor.putString("mRobotId", mRobot.getId().toString());
+            editor.putString("mRobotName", mRobot.getName());
+            editor.putInt("mRobotAge", mRobot.getAge());
+            editor.putInt("mRobotHappy", mRobot.getHappy());
+            editor.putInt("mRobotHunger", mRobot.getHunger());
+            editor.putInt("mRobotFatigue", mRobot.getFatigue());
+            editor.putInt("mRobotNaughty", mRobot.getNaughty());
+            editor.putInt("mRobotWaste", mRobot.getWaste());
+            editor.putBoolean("mRobotIllness", mRobot.getIllness());
+            editor.apply();
         }
     }
 }
