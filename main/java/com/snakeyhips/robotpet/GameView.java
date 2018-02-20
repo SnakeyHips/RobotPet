235276b123e54c0098eef896f2b9790108e0957c
Jake Gough <super.jake@hotmail.co.uk>
2018-02-20 10:20:36 +0000

    Update GameView.java

--- main/java/com/snakeyhips/robotpet/GameView.java
+++ main/java/com/snakeyhips/robotpet/GameView.java
@@ -2,4 +2,26 @@ package com.snakeyhips.robotpet;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback{
   
+  private MainThread thread;
+
+  public GameView(Context context) {
+      super(context);
+
+      getHolder().addCallback(this);
+  }
+  
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+  }
+
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+
+  }
+
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+
+  }
 }
