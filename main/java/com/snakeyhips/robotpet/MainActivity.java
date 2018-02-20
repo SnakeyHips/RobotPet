7950bd9f785756285cf8a5a8f9f153e258927c98
Jake Gough <super.jake@hotmail.co.uk>
2018-02-20 09:18:08 +0000

    Update MainActivity.java

--- main/java/com/snakeyhips/robotpet/MainActivity.java
+++ main/java/com/snakeyhips/robotpet/MainActivity.java
@@ -9,5 +9,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewByID(R.id.mToolbar));
+        
+        //Set ActionBar Title
+        getSupportActionBar().setTitle("Title Test");
+        
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                newGame();
+                return true;
+            case R.id.help:
+                showHelp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
