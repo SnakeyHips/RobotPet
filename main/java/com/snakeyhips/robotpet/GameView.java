ffd2647173896f457427ef66c366b69145d8b140
Jake Gough <super.jake@hotmail.co.uk>
2018-02-20 10:30:41 +0000

    Update GameView.java

--- main/java/com/snakeyhips/robotpet/GameView.java
+++ main/java/com/snakeyhips/robotpet/GameView.java
@@ -8,6 +8,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
       super(context);
 
       getHolder().addCallback(this);
+      thread = new MainThread(getHolder(), this);
+      setFocusable(true);
   }
   
   @Override
@@ -17,11 +19,36 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
   @Override
   public void surfaceCreated(SurfaceHolder holder) {
-
+    thread.setRunning(true);
+    tread.start();
   }
 
   @Override
   public void surfaceDestroyed(SurfaceHolder holder) {
+    boolean retry = true;
+    while(retry) {
+      try {
+        thread.setRunning(false);
+        thread.join();  
+      } catch(InterruptedException e) {
+          e.printStackTrace();
+      }
+      retry = false;
+    }
+  }
+  
+  public void update() {
 
+  }
+  
+  @Override
+  public void draw(Canvas canvas) {
+    super.draw(canvas);
+    if(canvas != null) {
+      canvas.drawColor(Color.WHITE);
+      Paint paint = new Paint();
+      paint.setColor(Color.rgb(250, 0, 0));
+      canvas.drawRect(100, 100, 200, 200, paint);
+    }
   }
 }
