a94839c6416e24cce80f32d2f2cacbb565a8890d
Jake Gough <super.jake@hotmail.co.uk>
2018-03-06 15:55:26 +0000

    Update ObstacleManager.java

--- main/java/com/snakeyhips/robotpet/ObstacleManager.java
+++ main/java/com/snakeyhips/robotpet/ObstacleManager.java
@@ -8,11 +8,15 @@ public class ObstacleManager {
     private int obstacleHeight;
     private int color;
     
+    private long startTime;
+    
     public ObstacleManager(int robotGap, int obstacleGap, int obstacleHeight, int color){
       this.robotGap = robotGap;
       this.obstacleGap = obstacleGap;
       this.obstacleHeight = obstacleHeight;
-      int.color = color;
+      this.color = color;
+        
+      startTime = System.currentTimeMillis();
       
       obstacles = new ArrayList<>();
       populateObstacles();
@@ -25,5 +29,25 @@ public class ObstacleManager {
         obstacles.add(new Obstacle(color, startX, currY, obstacleHeight, robotGap));
         currY += obstacleHeight + obstacleGap;
       }
+    }
+    
+    public void update(){
+        int elaspedTime = (int)(System.currentTimeMillis() - startTime);
+        startTime = System.currentTimeMillis();
+        float speed = Constants.SCREEN_HEIGHT/10000.0f;
+        for(Obstacle ob : obstacles){
+            ob.incrementY(speed * elaspedTime);
+        }
+        if(obstacles.get(obstacles.size - 1).getRect().top >= Constants.SCREEN_HEIGHT){
+            int startX = (int)(Math.random()*(Constants.SCREEN_WIDTH - robotGap));
+            obstacles.add(0, new Obstacle(color, startX,
+            obstacles.get(0).getRect().top + obstacleHeight + obstacleGap, obstacleHeight, robotGap));
+        }
+    }
+    
+    public void draw(Canvas canvas){
+        for(Obstacle ob : obstacles){
+            ob.draw(canvas);
+        }
     }
 }
