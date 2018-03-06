1484f26189ba917d4a497d92e76a8fb3697493f7
Jake Gough <super.jake@hotmail.co.uk>
2018-03-06 12:18:54 +0000

    Update RobotSprite.java

--- main/java/com/snakeyhips/robotpet/RobotSprite.java
+++ main/java/com/snakeyhips/robotpet/RobotSprite.java
@@ -3,18 +3,31 @@ package com.snakeyhips.robotpet;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
-public class RobotSprite{
-    private Bitmap sprite;
+public class RobotSprite implements GameObject{
+    //private Bitmap sprite;
+    private Rect rect;
+    private int color;
     
-    public RobotSprite(Bitmap bmp) {
-        sprite = bmp;
+    public RobotSprite(Rect rect, int color) {
+        this.rect = rect;
+        this.color = color
     }
     
+    
     public void draw(Canvas canvas){
-        canvas.drawBitmap(sprite, 100, 100, null);
+        //canvas.drawBitmap(sprite, 100, 100, null);
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rect, paint);
     }
     
     public void update(){
         //put stuff in here
+    }
+    
+    public void update(Point point){
+        //l, t, r, b
+        rect.set(point.x - rect.width()/2, point.y - rect.height()/2,
+                 point.x + rect.width()/2, point.y + rect.height()/2);
     }
 }
