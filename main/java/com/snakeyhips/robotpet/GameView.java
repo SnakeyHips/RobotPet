d6db58baf8f2350417488a3d54e6a3ebb805a9fb
Jake Gough <super.jake@hotmail.co.uk>
2018-03-08 08:40:05 +0000

    Update GameView.java

--- main/java/com/snakeyhips/robotpet/GameView.java
+++ main/java/com/snakeyhips/robotpet/GameView.java
@@ -16,14 +16,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
   private MainThread thread;
   private RobotSprite robotSprite;
   private Point robotPoint;
-  private ObstacleManager obstacleManager;
   private boolean movingPlayer = false;
   private boolean gameOver = false;
-  private long gameOverTime;
-  private Rect r = new Rect();
- 
-  //private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-  //private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
 
   public GameView(Context context, AttributeSet attrs) {
       super(context, attrs);
@@ -33,7 +27,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
       robotSprite = new RobotSprite(new Rect(100, 100, 200, 200), Color.rgb(255, 0, 0));
       robotPoint = new Point(Constans.SCREEN_WIDTH/2, 3*Constants.SCREEN_HEIGHT/4);
       robotSprite.update(robotPoint);
-      obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
     
       setFocusable(true);
   }
@@ -41,7 +34,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
   public void reset(){
       robotPoint = new Point(Constants.SCREEN_WIDTH/2, 3*Constants.SCREEN_HEIGHT/4);
       robotSprite.update(robotPoint);
-      obstacleManager = new ObstacleManager(200, 350, 75, Color.BLACK);
       movingPlayer = false;
   }
   
@@ -52,7 +44,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
 
   @Override
   public void surfaceCreated(SurfaceHolder holder) {
-    thread = new MainThread(getHolder(), this);
     thread.setRunning(true);
     thread.start();
   }
@@ -76,60 +67,23 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback{
   public boolean onTouchEvent(MotionEvent event){
       switch(event.getAction()){
         case MotionEvent.ACTION_DOWN:
-          if(!gameOver && robot.getRect().contains((int)event.getX(), (int)event.getY()){
-            movingPlayer = true;
-          }
-          if(gameOver && System.currentTimeMillis() - gameOverTime >= 2000){
-            reset();
-            gameOver = false;
-          }
-          break;
         case MotionEvent.ACTION_MOVE:
-          if(!gameOver && movingPlayer){
             robotPoint.set((int)event.getX(), (int)event.getY()); 
-          }
-          break;
-        case MotionEvent.ACTION_UP:
-             movingPlayer = false;
-             break;
+            break;
       }
       return true;
   }
   
   public void update() {
-    if(!gameOver){
       robotSprite.update(robotPoint);
-      obstacleManager.update();
-      if(obstacleManager.playerCollide(robot)){
-        gameOver = true;
-        gameOvertime = System.currentTimeMillis();
-      }
-    }
   }
   
   @Override
   public void draw(Canvas canvas) {
-    super.draw(canvas);
-    canvas.drawColor(Color.WHITE);
-    robotSprite.draw(canvas);
-    obstacleManager.draw(canvas);
-    
-    if(gameOver){
-      Paint paint = new Paint();
-      paint.setTextSize(100);
-      paint.setColor(Color.MAGENTA);
-      drawCenterText(canvas, paint, "Game Over");
+    if(canvas != null{
+        super.draw(canvas);
+        canvas.drawColor(Color.WHITE);
+        robotSprite.draw(canvas);
     }
-  }
-             
-  private void drawCenterText(Canvas canvas, Paint paint, String text) {
-    paint.setTextAlign(Paint.Align.LEFT);
-    canvas.getClipBounds(r);
-    int cHeight = r.height();
-    int cWidth = r.width();
-    paint.getTextBounds(text, 0, text.length(), r);
-    float x = cWidth / 2f - r.width() / 2f - r.left;
-    float y = cHeight / 2f + r.height() / 2f - r.bottom;
-    canvas.drawText(text, x, y, paint);
-  }           
+  }       
 }
