c4c63b37b8a9ca47ee5ed04ae941856d73ed368b
Jake Gough <super.jake@hotmail.co.uk>
2018-03-14 10:56:49 +0000

    Update MainActivity.java

--- main/java/com/snakeyhips/robotpet/MainActivity.java
+++ main/java/com/snakeyhips/robotpet/MainActivity.java
@@ -15,8 +15,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.UUID;
-
 public class MainActivity extends AppCompatActivity {
     
     //UI object references
