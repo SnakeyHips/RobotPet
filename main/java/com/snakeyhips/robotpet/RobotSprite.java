36b19d4fc082a9b7bea2d972f21c3880c77ae15c
Jake Gough <super.jake@hotmail.co.uk>
2018-03-16 11:48:51 +0000

    Update RobotSprite.java
    Trying out new method of animating sprite that moves around. Might go tits up

--- main/java/com/snakeyhips/robotpet/RobotSprite.java
+++ main/java/com/snakeyhips/robotpet/RobotSprite.java
@@ -6,54 +6,146 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 
-public class RobotSprite{
-    private Bitmap sprite;
-    private int x;
-    private int y;
-    
-    //Controlling X coordinate so that robot won't go outside the screen
-    private int maxX;
-    private int minX;
-    
-    public RobotSprite(Bitmap sprite, int x, int y, maxX, minX) {
-        this.sprite = sprite;
-        this.x = x;
-        this.y = y;
-    }
-    
-    public Bitmap getBitmap() {
-        return bitmap;
-    }
-    
-    public void setBitmap(Bitmap sprite) {
-        this.sprite = sprite;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+ 
+public class RobotSprite extends GameObject {
+ 
+    private static final int ROW_TOP_TO_BOTTOM = 0;
+    private static final int ROW_RIGHT_TO_LEFT = 1;
+    private static final int ROW_LEFT_TO_RIGHT = 2;
+    private static final int ROW_BOTTOM_TO_TOP = 3;
+ 
+    // Row index of Image are being used.
+    private int rowUsing = ROW_LEFT_TO_RIGHT;
+ 
+    private int colUsing;
+ 
+    private Bitmap[] leftToRights;
+    private Bitmap[] rightToLefts;
+    private Bitmap[] topToBottoms;
+    private Bitmap[] bottomToTops;
+ 
+    // Velocity of game character (pixel/millisecond)
+    public static final float VELOCITY = 0.1f;
+ 
+    private int movingVectorX = 10;
+    private int movingVectorY = 5;
+ 
+    private long lastDrawNanoTime =-1;
+ 
+    private GameView gameView;
+ 
+    public ChibiCharacter(GameView gameView, Bitmap image, int x, int y) {
+        super(image, 4, 3, x, y);
+ 
+        this.gameView= gameView;
+ 
+        this.topToBottoms = new Bitmap[colCount]; // 3
+        this.rightToLefts = new Bitmap[colCount]; // 3
+        this.leftToRights = new Bitmap[colCount]; // 3
+        this.bottomToTops = new Bitmap[colCount]; // 3
+ 
+        for(int col = 0; col< this.colCount; col++ ) {
+            this.topToBottoms[col] = this.createSubImageAt(ROW_TOP_TO_BOTTOM, col);
+            this.rightToLefts[col]  = this.createSubImageAt(ROW_RIGHT_TO_LEFT, col);
+            this.leftToRights[col] = this.createSubImageAt(ROW_LEFT_TO_RIGHT, col);
+            this.bottomToTops[col]  = this.createSubImageAt(ROW_BOTTOM_TO_TOP, col);
+        }
     }
  
-    public int getX() {
-        return x;
-    }
-    
-    public void setX(int x) {
-        this.x = x;
-    }
-    
-    public int getY() {
-        return y;
-    }
-      
-    public void draw(Canvas canvas){
-        //canvas.drawBitmap(sprite, 100, 100, null);
-        Paint paint = new Paint();
-        paint.setColor(color);
-        canvas.drawRect(rect, paint);
-    }
-    
-    public void update(Point point){
-        if (x < minX) {
-            x = minX;
+    public Bitmap[] getMoveBitmaps()  {
+        switch (rowUsing)  {
+            case ROW_BOTTOM_TO_TOP:
+                return  this.bottomToTops;
+            case ROW_LEFT_TO_RIGHT:
+                return this.leftToRights;
+            case ROW_RIGHT_TO_LEFT:
+                return this.rightToLefts;
+            case ROW_TOP_TO_BOTTOM:
+                return this.topToBottoms;
+            default:
+                return null;
         }
-        if (x > maxX) {
-            x = maxX;
+    }
+ 
+    public Bitmap getCurrentMoveBitmap()  {
+        Bitmap[] bitmaps = this.getMoveBitmaps();
+        return bitmaps[this.colUsing];
+    }
+ 
+ 
+    public void update()  {
+        this.colUsing++;
+        if(colUsing >= this.colCount)  {
+            this.colUsing =0;
         }
+        // Current time in nanoseconds
+        long now = System.nanoTime();
+ 
+        // Never once did draw.
+        if(lastDrawNanoTime==-1) {
+            lastDrawNanoTime= now;
+        }
+        // Change nanoseconds to milliseconds (1 nanosecond = 1000000 milliseconds).
+        int deltaTime = (int) ((now - lastDrawNanoTime)/ 1000000 );
+ 
+        // Distance moves
+        float distance = VELOCITY * deltaTime;
+ 
+        double movingVectorLength = Math.sqrt(movingVectorX* movingVectorX + movingVectorY*movingVectorY);
+ 
+        // Calculate the new position of the game character.
+        this.x = x +  (int)(distance* movingVectorX / movingVectorLength);
+        this.y = y +  (int)(distance* movingVectorY / movingVectorLength);
+ 
+        // When the game's character touches the edge of the screen, then change direction
+ 
+        if(this.x < 0 )  {
+            this.x = 0;
+            this.movingVectorX = - this.movingVectorX;
+        } else if(this.x > this.gameView.getWidth() -width)  {
+            this.x= this.gameView.getWidth()-width;
+            this.movingVectorX = - this.movingVectorX;
+        }
+ 
+        if(this.y < 0 )  {
+            this.y = 0;
+            this.movingVectorY = - this.movingVectorY;
+        } else if(this.y > this.gameView.getHeight()- height)  {
+            this.y= this.gameView.getHeight()- height;
+            this.movingVectorY = - this.movingVectorY ;
+        }
+ 
+        // rowUsing
+        if( movingVectorX > 0 )  {
+            if(movingVectorY > 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_TOP_TO_BOTTOM;
+            }else if(movingVectorY < 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_BOTTOM_TO_TOP;
+            }else  {
+                this.rowUsing = ROW_LEFT_TO_RIGHT;
+            }
+        } else {
+            if(movingVectorY > 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_TOP_TO_BOTTOM;
+            }else if(movingVectorY < 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_BOTTOM_TO_TOP;
+            }else  {
+                this.rowUsing = ROW_RIGHT_TO_LEFT;
+            }
+        }
+    }
+ 
+    public void draw(Canvas canvas)  {
+        Bitmap bitmap = this.getCurrentMoveBitmap();
+        canvas.drawBitmap(bitmap,x, y, null);
+        // Last draw time.
+        this.lastDrawNanoTime= System.nanoTime();
+    }
+ 
+    public void setMovingVector(int movingVectorX, int movingVectorY)  {
+        this.movingVectorX= movingVectorX;
+        this.movingVectorY = movingVectorY;
     }
 }
