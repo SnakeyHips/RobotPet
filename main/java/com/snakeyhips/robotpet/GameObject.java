5b52e5692f0f4ca1c23c790e8bf4fc3dba6e393d
Jake Gough <super.jake@hotmail.co.uk>
2018-03-20 09:05:56 +0000

    Update GameObject.java

--- main/java/com/snakeyhips/robotpet/GameObject.java
+++ main/java/com/snakeyhips/robotpet/GameObject.java
@@ -1,49 +1,202 @@
 package com.snakeyhips.robotpet;
 
+import android.content.Context;
 import android.graphics.Bitmap;
- 
-public abstract class GameObject {
- 
-    protected Bitmap image;
-    protected final int rowCount;
-    protected final int colCount;
-    protected final int WIDTH;
-    protected final int HEIGHT;
-    protected final int width; 
-    protected final int height;
-    protected int x;
-    protected int y;
- 
-    public GameObject(Bitmap image, int rowCount, int colCount, int x, int y)  {
-        this.image = image;
-        this.rowCount = rowCount;
-        this.colCount = colCount;
-        this.x = x;
-        this.y = y;
-        this.WIDTH = image.getWidth();
-        this.HEIGHT = image.getHeight();
-        this.width = this.WIDTH/ colCount;
-        this.height = this.HEIGHT/ rowCount;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+public class GameObject {
+
+    private Rect rect;
+    private Animation idle;
+    private Animation walkRight;
+    private Animation walkLeft;
+    private AnimationManager animManager;
+
+    public GameObject(Context context, Rect rect, Bitmap idleImg, Bitmap walk1, Bitmap walk2){
+        this.rect = rect;
+        
+        //Create animations from bitmaps
+        idle = new Animation(new Bitmap[]{idleImg}, 2);
+        walkRight = new Animation(new Bitmap[]{walk1, walk2}, 0.5f);
+        walkLeft = new Animation(new Bitmap[]{Animation.reverse(walk1);, Animation.reverse(walk2)}, 0.5f);
+        animManager = new AnimationManager(new Animation[]{idle, walkRight, walkLeft});
     }
-  
-    protected Bitmap createSubImageAt(int row, int col)  {
-        Bitmap subImage = Bitmap.createBitmap(image, col * width, row * height, width, height);
-        return subImage;
+
+    public Rect getRect(){
+        return rect;
+    }
+
+    public void draw(Canvas canvas){
+        animManager.draw(canvas, rect);
+    }
+
+    public void update(){
+        animManager.update();
+    }
+
+    public void update(Point point){
+        float oldLeft = rect.left;
+
+        rect.set(point.x - rect.width()/2, point.y - rect.height()/2,
+                point.x + rect.width()/2, point.y + rect.height()/2);
+
+        int state = 0;
+        if(rect.left - oldLeft > 5){
+            state = 1;
+        } else if(rect.left - oldLeft < -5){
+            state = 2;
+        }
+
+        animManager.playAnim(state);
+        animManager.update();
+    }
+    
+    public void setMovingVector(int movingVectorX, int movingVectorY)  {
+        this.movingVectorX = movingVectorX;
+        this.movingVectorY = movingVectorY;
+    }
+
+    /* vector method
+    private static final int ROW_TOP_TO_BOTTOM = 0;
+    private static final int ROW_RIGHT_TO_LEFT = 1;
+    private static final int ROW_LEFT_TO_RIGHT = 2;
+    private static final int ROW_BOTTOM_TO_TOP = 3;
+ 
+    // Row index of Image are being used.
+    private int rowUsing = ROW_LEFT_TO_RIGHT;
+ 
+    private int colUsing;
+ 
+    private Bitmap[] leftToRights;
+    private Bitmap[] rightToLefts;
+    private Bitmap[] topToBottoms;
+    private Bitmap[] bottomToTops;
+ 
+    // Velocity of game character (pixel/millisecond)
+    public static final float VELOCITY = 0.05f;
+ 
+    private int movingVectorX = 10;
+    private int movingVectorY = 5;
+ 
+    private long lastDrawNanoTime =-1;
+ 
+    private GameView gameView;
+ 
+    public RobotSprite(GameView gameView, Bitmap image, int x, int y) {
+        super(image, 4, 3, x, y);
+ 
+        this.gameView= gameView;
+ 
+        this.topToBottoms = new Bitmap[colCount]; // 3
+        this.rightToLefts = new Bitmap[colCount]; // 3
+        this.leftToRights = new Bitmap[colCount]; // 3
+        this.bottomToTops = new Bitmap[colCount]; // 3
+ 
+        for(int col = 0; col< this.colCount; col++ ) {
+            this.topToBottoms[col] = this.createSubImageAt(ROW_TOP_TO_BOTTOM, col);
+            this.rightToLefts[col]  = this.createSubImageAt(ROW_RIGHT_TO_LEFT, col);
+            this.leftToRights[col] = this.createSubImageAt(ROW_LEFT_TO_RIGHT, col);
+            this.bottomToTops[col]  = this.createSubImageAt(ROW_BOTTOM_TO_TOP, col);
+        }
     }
  
-    public int getX()  {
-        return x;
+    public Bitmap[] getMoveBitmaps()  {
+        switch (rowUsing)  {
+            case ROW_BOTTOM_TO_TOP:
+                return  this.bottomToTops;
+            case ROW_LEFT_TO_RIGHT:
+                return this.leftToRights;
+            case ROW_RIGHT_TO_LEFT:
+                return this.rightToLefts;
+            case ROW_TOP_TO_BOTTOM:
+                return this.topToBottoms;
+            default:
+                return null;
+        }
     }
  
-    public int getY()  {
-        return y;
+    public Bitmap getCurrentMoveBitmap()  {
+        Bitmap[] bitmaps = this.getMoveBitmaps();
+        return bitmaps[this.colUsing];
     }
  
-    public int getHeight() {
-        return height;
+ 
+    public void update()  {
+        this.colUsing++;
+        if(colUsing >= this.colCount)  {
+            this.colUsing =0;
+        }
+        // Current time in nanoseconds
+        long now = System.nanoTime();
+ 
+        // Never once did draw.
+        if(lastDrawNanoTime == -1) {
+            lastDrawNanoTime = now;
+        }
+        // Change nanoseconds to milliseconds (1 nanosecond = 1000000 milliseconds).
+        int deltaTime = (int) ((now - lastDrawNanoTime) / 1000000);
+ 
+        // Distance moves
+        float distance = (VELOCITY * deltaTime);
+ 
+        double movingVectorLength = Math.sqrt(movingVectorX * movingVectorX + movingVectorY * movingVectorY);
+ 
+        // Calculate the new position of the game character.
+        this.x += (int)(distance * movingVectorX / movingVectorLength);
+        this.y += (int)(distance * movingVectorY / movingVectorLength);
+ 
+        // When the game's character touches the edge of the screen, then change direction
+ 
+        if(this.x < 0 )  {
+            this.x = 0;
+            this.movingVectorX = -this.movingVectorX;
+        } else if(this.x > this.gameView.getWidth() - width)  {
+            this.x = this.gameView.getWidth() - width;
+            this.movingVectorX = -this.movingVectorX;
+        }
+ 
+        if(this.y < 0 )  {
+            this.y = 0;
+            this.movingVectorY = -this.movingVectorY;
+        } else if(this.y > this.gameView.getHeight() - height)  {
+            this.y = this.gameView.getHeight() - height;
+            this.movingVectorY = -this.movingVectorY;
+        }
+ 
+        // rowUsing
+        if( movingVectorX > 0 )  {
+            if(movingVectorY > 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_TOP_TO_BOTTOM;
+            }else if(movingVectorY < 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_BOTTOM_TO_TOP;
+            }else  {
+                this.rowUsing = ROW_LEFT_TO_RIGHT;
+            }
+        } else {
+            if(movingVectorY > 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_TOP_TO_BOTTOM;
+            }else if(movingVectorY < 0 && Math.abs(movingVectorX) < Math.abs(movingVectorY)) {
+                this.rowUsing = ROW_BOTTOM_TO_TOP;
+            }else  {
+                this.rowUsing = ROW_RIGHT_TO_LEFT;
+            }
+        }
     }
  
-    public int getWidth() {
-        return width;
+    public void draw(Canvas canvas)  {
+        Bitmap bitmap = this.getCurrentMoveBitmap();
+        canvas.drawBitmap(bitmap, x, y, null);
+        // Last draw time.
+        this.lastDrawNanoTime = System.nanoTime();
     }
+ 
+    public void setMovingVector(int movingVectorX, int movingVectorY)  {
+        this.movingVectorX = movingVectorX;
+        this.movingVectorY = movingVectorY;
+    }*/
 }
